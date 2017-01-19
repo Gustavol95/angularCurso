d7d555928e6eb32fd50d325dbda937b3a266185b
GustavoL95 <tavols889@gmail.com>
2017-01-18 23:51:02 -0700

    x

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -5,17 +5,21 @@ import {Routes, RouterModule} from "@angular/router";
 import { FormClientesComponent } from './form-clientes/form-clientes.component';
 import {UpdateClientesComponent} from "./form-clientes/update-clientes.component";
 import { ModalComponent } from './modal/modal.component';
+import { EditarClientesComponent } from './editar-clientes/editar-clientes.component';
+import {SharedModule} from "../shared/shared.module";
 
 const routes:Routes =[
   {path:'',component : ListClientesComponent},
   {path:'create',component : FormClientesComponent},
-  {path: ':id', component:UpdateClientesComponent}
+  {path: ':id', component:UpdateClientesComponent},
+  {path: 'go', component:EditarClientesComponent}
 ]
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ListClientesComponent, FormClientesComponent, UpdateClientesComponent, ModalComponent]
+  declarations: [ListClientesComponent, FormClientesComponent, UpdateClientesComponent, ModalComponent, EditarClientesComponent]
 })
 export class ClientesModule { }
--- src/app/clientes/list-clientes/list-clientes.component.ts
+++ src/app/clientes/list-clientes/list-clientes.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {ClienteService} from "../cliente.service";
 import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {isNullOrUndefined} from "util";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list-clientes',
@@ -12,19 +14,34 @@ export class ListClientesComponent implements OnInit {
   private _clienteService : ClienteService;
   private _clientes: any[]=[];
   closeResult: string;
-  constructor(clienteService : ClienteService,private modalService: NgbModal ) {
+  constructor(clienteService : ClienteService,private router:Router  ) {
     this._clienteService=clienteService;
   }
 
   ngOnInit() {
-    this._clienteService.getAll().subscribe(res=>this._clientes=res.data);
+
+    this._clientes=JSON.parse(localStorage.getItem("listado"));
+    if(isNullOrUndefined(this._clientes)){
+      console.log("Null or undefined "+localStorage.getItem("listado"));
+      this.retrieveClients();
+    }
+  }
+
+  retrieveClients(){
+    this._clienteService.getAll().subscribe(res=>{this._clientes=res.data;
+    localStorage.setItem("listado",JSON.stringify(this._clientes))});
   }
 
   onEliminarClick(element){
     console.log("clickeado en"+this._clientes.indexOf(element));
     this._clientes.splice(this._clientes.indexOf(element),1);
+    localStorage.setItem("listado",JSON.stringify(this._clientes));
   }
-  open(content) {
-    this.modalService.open(content, { windowClass: 'dark-modal' });
+
+
+  editar(element){
+    console.log(this._clientes[this._clientes.indexOf(element)]);
+    localStorage.setItem("editar", this._clientes[this._clientes.indexOf(element)])
+    this.router.navigateByUrl("go");
   }
 }
